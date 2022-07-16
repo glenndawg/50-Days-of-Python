132e6130e2692de252fc0e92291e1067c210ba87
Glenn MacRae <108369575+glenndawg@users.noreply.github.com>
2022-07-16 12:04:26 -0700

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,23 @@
+# DAY 2 : STRINGS TO INTEGERS
+# Write a function called convert_add that takes a list of strings as an
+# argument and converts it to integers and summs the list. For Example
+# ['1','3','5'] should be converted to [1,3,5] and summed to 9.
+
+
+def convert_add(list_int):
+    list_int = list(map(int, list_int))
+    total = 0
+    total = sum(list_int)
+    print("The Sum of the list is: ", total)
+    return list_int
+
+list_str = ['1','2','3','4','5','6']
+
+
+print("Original List  :", (list_str))  
+print("The new List is: ",convert_add(list_str))
+
+
+# Question to improve code :
+# Can I return the total, and list_int
+# How else can I print the sum of the new list outside of the function?
