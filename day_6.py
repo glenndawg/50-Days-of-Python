4b50d6202579118459b7c0a6263e1ea34df5a816
Glenn MacRae <glennie@gmail.com>
2022-07-19 14:45:12 -0700

    added day_6 to the repository

---
+++ day_6.py
@@ -0,0 +1,25 @@
+# DAY 6 : USER NAME GENERATOR
+# Write a function called user_name that generates a username from 
+# the user's email. The code should ask the user to input an email and
+# the code should return everything before the @ sign as their user 
+# name. For example, if someone enters ben@gmail.com, the code 
+# should return ben as their user name.
+
+import re
+def user_name(email):
+    name = email.split('@')[0]
+    return name
+
+email = input("Please enter you email address : ")
+
+
+print(f"Your user name is : {user_name(email)}")
+
+# How so I ject the text to see if it in fact 
+# an email address?
+
+
+# Another method, but less efficient is :
+# name = email.rpartition('@'[0])
+# retuen name[0]
+
