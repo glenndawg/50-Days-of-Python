3c26ce0f89bfa6c60d5c129047e91d3add4c3daf
Glenn MacRae <glennie@gmail.com>
2022-07-20 10:52:00 -0700

    dat 7

---
+++ day_7.py
@@ -0,0 +1,31 @@
+# DAY 7 : A strring Range
+# Write a function called string_range that takes a single number and
+# returns a string of its range. The string characters should be 
+# seperated by dots(.). For example, if you pass 6 as an argument, 
+# your functiomn should return '0.1.2.3.4.5'
+
+
+
+def string_range(num):
+    string = ""
+    count = 0
+    while count < num:
+        if count < num - 1:
+            string = string + (str(count) + ".")
+        else:
+            string = string + (str(count))
+        count += 1
+    return(string)
+    
+while True:
+    try:
+        num = int(input("Please enter an integere : "))
+        break
+    except ValueError:
+        print("Please enter a whole number")
+
+print(string_range(num))
+
+# works great! Whats the better way ??
+
+
