06e6e61d66760c3284e609dba9b10dab6105a6f8
Glenn MacRae <glennie@gmail.com>
2022-07-21 20:38:38 -0700

    new file

---
+++ day_12.py
@@ -0,0 +1,37 @@
+# DAY 12 : COUNT THE DOTS
+# Write a function called count_dots. This function takes a string
+# seperated by dots as a parameter and counts how many dots are in
+# the string. For examlpe, 'h.e.l.p.' should return 4 dots, and 'he.lp'
+# should return 2 dots.
+
+string = 'h.e.l.p.'
+
+def count_dots(string):
+    count = string.count('.')
+    return count
+
+print(count_dots(string))
+
+
+# here is the list comprehension method
+# sum = [x for x in string if x == '.']
+# count = len(sum)
+
+# you can also us the filter() function instead with a lambda
+# expression instead of list comprehension
+# sum = list(filter(lambda x: x == '.', string))
+
+# here is the long version using for loop
+# string = 'h.e.l.p'
+#
+# def count_dots(string):
+#     count = 0
+#     for x in range(len(string)):
+#         if "." == string[x]:
+#             count += 1
+#             continue
+#     return count
+#
+# print(count_dots(string))
+#
+# sum = [x for x in string if x = '.']
