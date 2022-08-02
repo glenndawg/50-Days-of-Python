759523913bf0dc5c1b5a5fb945bb9af6b0883943
Glenn MacRae <glennie@gmail.com>
2022-08-02 16:45:44 -0700

    update

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-import itertools
-   
-# for in loop
-for i in itertools.count(5, 5):
-    if i == 35:
-        break
-    else:
-        print(i, end =" ")
+import csv
 
+file = "emai_list.csv"
+fields = ['email']
+email_list = []
+row = None
+
+while True:
+    row = input("Please enter an email (enter done when finished) :")
+    # check here for email format
+    if row in ('done','DONE'): break
+    email_list.append(row)
+
+with open('email_list.csv', 'w', encoding='UTF8') as csvfile:
+    writer = csv.writer(csvfile)
+    for i in range(len(email_list)):
+        writer.writerow(email_list[i])
+
+print(email_list)
