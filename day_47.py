8cf23fbec8a210ea5102ddec2c6078b4dac56bbc
Glenn MacRae <glennie@gmail.com>
2022-08-02 18:14:57 -0700

    update

---
+++ day_47.py
@@ -0,0 +1,22 @@
+# Day 47: Save a JSON
+# Write a function called save_json. This function takes a dictionary
+# below as an argument and saves it on a file in JSON format.
+# Write another function called read_json that opens the file that you
+# just saved and reads its content.
+import json
+
+names = {'name': 'Carol','sex': 'female','age': 55}
+
+def save_json():
+    with open('data.json', 'w') as file:
+        obj = json.dumps(names, indent = 4)
+        json.dump(obj, file)
+        return obj
+
+def read_json():
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+        return data
+
+print(save_json())
+print(read_json())
